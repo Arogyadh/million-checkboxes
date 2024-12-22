ff81ba9b914d2c9dd789e279b54a4b80973eac01
Arogya Dhaubhadel <arogyadhdl@gmail.com>
2024-12-22 22:31:30 +0545

    added fixed height for scroller to work, hide the scroller

--- resources/js/components/Scroller.vue
+++ resources/js/components/Scroller.vue
@@ -1,23 +1,35 @@
 <script setup>
 import { RecycleScroller } from "vue-virtual-scroller";
+import { ref } from "vue";
+
 let items = [...Array(1000000).keys()].map((index) => {
     return {
         id: index + 1,
         checked: false,
     };
 });
+
+const gridItems = ref(32);
+const itemSize = ref(1232 / gridItems.value);
 </script>
 
 <template>
     <RecycleScroller
+        class="flex-grow overflow-y-auto max-h-[720px] scrollbar-hide"
         :items="items"
-        :item-size="10"
-        class="bg-blue-500 flex-grow overflow-y-auto"
+        :item-size="itemSize"
+        :grid-items="gridItems"
     >
         <template #default="{ item }">
-            <div>
-                {{ item }}
+            <div class="h-full flex items-center justify-center">
+                <input type="checkbox" class="size-6" :checked="item.checked" />
             </div>
         </template>
     </RecycleScroller>
 </template>
+
+<style scoped>
+.scrollbar-hide::-webkit-scrollbar {
+    display: none;
+}
+</style>
--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,13 +1,15 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name') }}</title>
-</head>
 
-@vite(['resources/js/app.js', 'resources/css/app.css'])
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
+</head>
 
 <body>
     <div id="app" class="max-w-7xl px-6 w-full mx-auto flex flex-col h-screen">
